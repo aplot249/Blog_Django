12b49946305ae07ec158808b17d373ba974485c4
wsuo <ws2821@yeah.net>
2020-06-13 19:13:54 +0800

    最终提交

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'py_blog',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': '****',
+        'PASSWORD': '****',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
