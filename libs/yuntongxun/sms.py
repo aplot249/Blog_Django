33fd85ac6010e8e46469a22a8d8c4f4a79e67e1f
wsuo <ws2821@yeah.net>
2020-06-13 18:56:20 +0800

    修改README

--- libs/yuntongxun/sms.py
+++ libs/yuntongxun/sms.py
@@ -7,13 +7,13 @@ from libs.yuntongxun.CCPRestSDK import REST
 # 8aaf0708568d4143015697b0f56e088f
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8aaf07087291adde0172a730d7860cde'
+_accountSid = '****'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '02fca208efab477ab0d87f248c805b6f'
+_accountToken = '****'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8aaf07087291adde0172a730d88c0ce5'
+_appId = '****'
 
 # sandboxapp.cloopen.com
 # 说明：请求地址，生产环境配置成app.cloopen.com
